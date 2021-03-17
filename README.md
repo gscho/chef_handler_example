78801d3f9ff367d1c3e584d311860b42616648b9
gscho <greg.c.schofield@gmail.com>
2021-03-17 11:58:06 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_handler_example
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_handler_example
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
